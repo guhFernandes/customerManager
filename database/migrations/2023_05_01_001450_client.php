d84feecde5b2cbaf9274f6b384f57bdaafc42a6a
Gustavo Fernandes <gfernandesilva2904@gmail.com>
2023-04-30 21:31:25 -0300

    criacao das migrations

---
+++ database/migrations/2023_05_01_001450_client.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('client', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('sobrenome');
+            $table->string('endereco');
+            $table->string('cpf',11);
+            $table->string('telefone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('client');
+    }
+};
